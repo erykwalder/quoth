85805d362d84095ce8d3dd16d2011e4f0804126f
Eric Rykwalder <e.rykwalder@gmail.com>
2021-08-28 17:13:38 -0400

    finish settings parsing

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -14,6 +14,84 @@ describe("parser", () => {
     expect(result.heading).toBe("#Lunar Cycles");
   });
 
+  it("parses the block id", () => {
+    const result: Embed = parse("block: ^foobar1");
+    expect(result.block).toBe("^foobar1");
+  });
+
+  it("parses a single number-based range", () => {
+    const result: Embed = parse("ranges: 5:10 to 7:15");
+    expect(result.ranges).toStrictEqual([
+      {
+        start: { line: 5, col: 10 },
+        end: { line: 7, col: 15 },
+      },
+    ]);
+  });
+
+  it("parses a single string-based range", () => {
+    const result: Embed = parse(`ranges: "Hello" to "world."`);
+    expect(result.ranges).toStrictEqual([
+      {
+        start: "Hello",
+        end: "world.",
+      },
+    ]);
+  });
+
+  it("parses a single mixed range", () => {
+    const result: Embed = parse(`ranges: "Hello" to 7:15`);
+    expect(result.ranges).toStrictEqual([
+      {
+        start: "Hello",
+        end: { line: 7, col: 15 },
+      },
+    ]);
+  });
+
+  it("parses multiple ranges", () => {
+    const result: Embed = parse(`ranges: "Hello" to 7:15, 9:13 to "foobar"`);
+    expect(result.ranges).toStrictEqual([
+      {
+        start: "Hello",
+        end: { line: 7, col: 15 },
+      },
+      {
+        start: { line: 9, col: 13 },
+        end: "foobar",
+      },
+    ]);
+  });
+
+  it("parses a join string", () => {
+    const result: Embed = parse(`join: "; "`);
+    expect(result.join).toBe("; ");
+  });
+
+  it("parses show options", () => {
+    const result: Embed = parse("show: title, author, created");
+    expect(result.show).toStrictEqual({
+      title: true,
+      author: true,
+      created: true,
+    });
+  });
+
+  it("defaults join to ...", () => {
+    const result: Embed = parse("");
+    expect(result.join).toBe(" ... ");
+  });
+
+  it("parses the display setting", () => {
+    const result: Embed = parse("display: inline");
+    expect(result.display).toBe("inline");
+  });
+
+  it("defaults the display setting to embedded", () => {
+    const result: Embed = parse("");
+    expect(result.display).toBe("embedded");
+  });
+
   it("parses multiple lines", () => {
     const text = `
 file: [[Once in a Blue Moon]]
@@ -22,5 +100,42 @@ heading: #Lunar Cycles
     const result: Embed = parse(text);
     expect(result.file).toBe("[[Once in a Blue Moon]]");
     expect(result.heading).toBe("#Lunar Cycles");
+  });
+
+  it("parses a whole block", () => {
+    const text = `file: [[File Title]]
+heading: #Heading#21
+block: ^asdf
+ranges: 123:10 to 125:10, 150:6 to "text", "start" to "end"
+join: ", "
+show: title, created
+display: inline`;
+    const result: Embed = parse(text);
+    expect(result).toStrictEqual({
+      file: "[[File Title]]",
+      heading: "#Heading#21",
+      block: "^asdf",
+      ranges: [
+        {
+          start: { line: 123, col: 10 },
+          end: { line: 125, col: 10 },
+        },
+        {
+          start: { line: 150, col: 6 },
+          end: "text",
+        },
+        {
+          start: "start",
+          end: "end",
+        },
+      ],
+      join: ", ",
+      show: {
+        title: true,
+        author: false,
+        created: true,
+      },
+      display: "inline",
+    });
   });
 });
--- src/parse.ts
+++ src/parse.ts
@@ -3,8 +3,9 @@ export interface Embed {
   heading?: string;
   block?: string;
   ranges?: EmbedRange[];
-  show?: EmbedOptions[];
-  display?: DisplayOptions;
+  join: string;
+  show: EmbedOptions;
+  display: "embedded" | "inline";
 }
 
 export interface EmbedRange {
@@ -17,16 +18,25 @@ export interface Pos {
   col: number;
 }
 
-export enum EmbedOptions {
-  TITLE,
-  AUTHOR,
-  CREATED,
+export interface EmbedOptions {
+  title: boolean;
+  author: boolean;
+  created: boolean;
 }
 
-export enum DisplayOptions {
-  EMBEDDED,
-  INLINE,
-}
+const parseRangeEndToken = (token: string): Pos | string => {
+  if (token[0] === '"') {
+    return JSON.parse(token) as string;
+  }
+  const nums = token.split(":");
+  if (nums.length != 2) {
+    throw new Error("invalid ranges line");
+  }
+  return {
+    line: parseInt(nums[0]),
+    col: parseInt(nums[1]),
+  };
+};
 
 type LineParser = (text: string, data: Embed) => void;
 
@@ -35,26 +45,103 @@ const lineParsers: { [key: string]: LineParser } = {
     // Match: [[text]]
     if (/^\[\[.+?\]\]$/.test(text)) {
       data.file = text;
+    } else {
+      throw new Error("invalid file line");
     }
   },
   heading: (text: string, data: Embed) => {
     // Match: #text
     if (/^#.+?$/.test(text)) {
       data.heading = text;
+    } else {
+      throw new Error("invalid heading line");
+    }
+  },
+  block: (text: string, data: Embed) => {
+    // Match: ^text
+    if (/^\^.+?$/.test(text)) {
+      data.block = text;
+    } else {
+      throw new Error("invalid block line");
+    }
+  },
+  ranges: (text: string, data: Embed) => {
+    // Match: "string" | 10:15 | to | ,
+    // g suffix: match all
+    const tokenRegex = /"([^"\\]|\\.)+"|\d+:\d+|to|,/g;
+    data.ranges = [];
+    const tokens: string[] = [];
+
+    let matches: string[];
+    while ((matches = tokenRegex.exec(text)) !== null) {
+      tokens.push(matches[0]);
+    }
+
+    let idx = 0;
+    while (idx + 2 < tokens.length) {
+      if (tokens[idx + 1] != "to") {
+        throw new Error("invalid ranges line");
+      }
+      data.ranges.push({
+        start: parseRangeEndToken(tokens[idx]),
+        end: parseRangeEndToken(tokens[idx + 2]),
+      });
+      if (idx + 3 < tokens.length && tokens[idx + 3] != ",") {
+        throw new Error("invalid ranges line");
+      }
+      idx += 4;
+    }
+  },
+  join: (text: string, data: Embed) => {
+    // Match: "text"
+    if (/^\"([^"\\]|\\.)+\"$/.test(text)) {
+      data.join = JSON.parse(text) as string;
+    } else {
+      throw new Error("invalid join line");
+    }
+  },
+  show: (text: string, data: Embed) => {
+    // Match: word, another, final
+    // g suffix: match all
+    const showRegex = /(^|,)\s*([a-z]+)\s*/g;
+    let matches: string[];
+    while ((matches = showRegex.exec(text)) !== null) {
+      const toggle = matches[2] as keyof EmbedOptions;
+      if (toggle in data.show) {
+        data.show[toggle] = true;
+      } else {
+        throw new Error("invalid show option");
+      }
+    }
+  },
+  display: (text: string, data: Embed) => {
+    // Match: embedded | inline
+    if (/^embedded|inline$/.test(text)) {
+      data.display = text as "embedded" | "inline";
+    } else {
+      throw new Error("invalid display line");
     }
   },
 };
 
-// Match: settingName: setting value
-// g suffix: match all
-// m suffix: ^ and $ work line by line, not on the whole string
-const SETTING_REGEX = /^(\w+):\s*(.+?)\s*$/gm;
-
 export const parse = (text: string): Embed => {
-  const embedData: Embed = {};
+  // Match: settingName: setting value
+  // g suffix: match all
+  // m suffix: ^ and $ work line by line, not on the whole string
+  const settingRegex = /^(\w+):\s*(.+?)\s*$/gm;
+
+  const embedData: Embed = {
+    join: " ... ",
+    show: {
+      title: false,
+      author: false,
+      created: false,
+    },
+    display: "embedded",
+  };
 
   let matches: string[];
-  while ((matches = SETTING_REGEX.exec(text)) !== null) {
+  while ((matches = settingRegex.exec(text)) !== null) {
     const [_, settingName, settingText] = matches;
     if (settingName in lineParsers) {
       lineParsers[settingName](settingText, embedData);
