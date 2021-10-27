b2340a10a861e125895ad7314523d3f365a59e33
Eric Rykwalder <e.rykwalder@gmail.com>
2021-10-27 17:42:11 -0400

    extract copyReference command

---
+++ src/copyReference.ts
@@ -0,0 +1,37 @@
+import { clipboard } from "electron";
+import { MarkdownView, Plugin } from "obsidian";
+import { isRepeated, uniqueStrRange } from "./stringSearch";
+
+export default function copyReference(checking: boolean) {
+  const view = this.app.workspace.activeLeaf.view as MarkdownView;
+  if (!checking) {
+    if (view) {
+      let ref = "```quoth\n";
+      ref += `file: [[${this.app.workspace.getActiveFile().path}]]\n`;
+
+      const selectedText = view.editor.getSelection();
+
+      if (!isRepeated(view.editor.getValue(), selectedText)) {
+        const points = uniqueStrRange(view.editor.getValue(), selectedText);
+        ref += `ranges: ${points.map((p) => JSON.stringify(p)).join(" to ")}\n`;
+      } else {
+        const selection = view.editor.listSelections()[0];
+        let start = selection.anchor;
+        let end = selection.head;
+        if (
+          start.line > end.line ||
+          (start.line == end.line && start.ch > end.ch)
+        ) {
+          [start, end] = [end, start];
+        }
+        ref += `ranges: ${start.line}:${start.ch} to ${end.line}:${end.ch}\n`;
+      }
+
+      ref += "```";
+
+      clipboard.writeText(ref);
+    }
+  }
+
+  return view && view.editor.somethingSelected();
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import {
   MarkdownPostProcessorContext,
   Plugin,
-  MarkdownView,
   MarkdownRenderer,
   TFile,
 } from "obsidian";
 import { parse } from "./parse";
-import { clipboard } from "electron";
-import { isRepeated, strRange, uniqueStrRange } from "./stringSearch";
+import { strRange } from "./stringSearch";
+import copyReference from "./copyReference";
 
 export default class QuothPlugin extends Plugin {
   async onload() {
@@ -16,44 +15,7 @@ export default class QuothPlugin extends Plugin {
     this.addCommand({
       id: "quoth-copy-reference",
       name: "Copy Reference",
-      checkCallback: (checking: boolean) => {
-        const view = this.app.workspace.activeLeaf.view as MarkdownView;
-        if (!checking) {
-          if (view) {
-            let ref = "```quoth\n";
-            ref += `file: [[${this.app.workspace.getActiveFile().path}]]\n`;
-
-            const selectedText = view.editor.getSelection();
-
-            if (!isRepeated(view.editor.getValue(), selectedText)) {
-              const points = uniqueStrRange(
-                view.editor.getValue(),
-                selectedText
-              );
-              ref += `ranges: ${points
-                .map((p) => JSON.stringify(p))
-                .join(" to ")}\n`;
-            } else {
-              const selection = view.editor.listSelections()[0];
-              let start = selection.anchor;
-              let end = selection.head;
-              if (
-                start.line > end.line ||
-                (start.line == end.line && start.ch > end.ch)
-              ) {
-                [start, end] = [end, start];
-              }
-              ref += `ranges: ${start.line}:${start.ch} to ${end.line}:${end.ch}\n`;
-            }
-
-            ref += "```";
-
-            clipboard.writeText(ref);
-          }
-        }
-
-        return view && view.editor.somethingSelected();
-      },
+      checkCallback: copyReference.bind(this),
     });
   }
 }
