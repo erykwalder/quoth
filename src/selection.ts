e855f6fd7d580e99146f575de3f48797de218230
Eric Rykwalder <e.rykwalder@gmail.com>
2021-12-19 23:59:50 -0500

    keep selection when opening mobile command palette
    Previously, selection was lost when tapping the command palette button.
    Updating lastRange should ignore body.is-mobile start elements.

--- src/selection.ts
+++ src/selection.ts
@@ -5,16 +5,20 @@ export default function getSelectedRange(): Range {
 }
 
 export function isTextSelected(): boolean {
-  return !getSelectedRange().collapsed;
+  return !getSelectedRange()?.collapsed;
 }
 
 export function selectListener() {
-  const sel = window.getSelection();
+  const sel = document.getSelection();
   if (sel.rangeCount > 0) {
-    const range = window.getSelection().getRangeAt(0);
+    const range = document.getSelection().getRangeAt(0);
+    const startEl = range.startContainer as HTMLElement;
     if (
-      !range.collapsed ||
-      (range.startContainer as HTMLElement)?.className != "prompt"
+      startEl?.className != "prompt" &&
+      !(
+        startEl?.tagName == "BODY" &&
+        startEl?.className?.indexOf("is-mobile") >= 0
+      )
     ) {
       lastRange = range;
     }
